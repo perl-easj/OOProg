5c7a0ff3476b4b2ed369529b06bf5ff74f3551d4
Per Storgaard Laursen <perl@easj.dk>
2019-05-10 13:39:21 +0200

    A few misc minor updates

---
+++ ASWC/MandAssign/ObservableInt/ObservableInt/PSL_ObsInt.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace ObservableInt
+{
+    public class PSL_ObsInt
+    {
+        private int _value;
+        public event Action<int> ValueChanged;
+
+        public PSL_ObsInt(int val = 0)
+        {
+            _value = val;
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                OnValueChanged(value);
+            }
+        }
+
+        protected virtual void OnValueChanged(int obj)
+        {
+            ValueChanged?.Invoke(obj);
+        }
+
+        public static implicit operator int(PSL_ObsInt oi)
+        {
+            return oi.Value;
+        }
+
+        public static implicit operator PSL_ObsInt(int val)
+        {
+            return new PSL_ObsInt(val);
+        }
+    }
+}
---
+++ ASWC/MandAssign/ObservableInt/ObservableInt/PSL_ObsIntExt.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObservableInt
+{
+    public static class PSL_ObsIntExt
+    {
+        public static int Smallest(this IEnumerable<PSL_ObsInt> collection)
+        {
+            return collection.Min(i => i.Value);
+        }
+
+        public static int Total(this IEnumerable<PSL_ObsInt> collection)
+        {
+            return collection.Sum(i => i.Value);
+        }
+
+        // TODO - implement ObsIntExt
+    }
+}
